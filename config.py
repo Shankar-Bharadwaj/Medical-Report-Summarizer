78c73442f628e137df093e851a1b36d53b7c5d77
Shankar Bharadwaj <bharadwajsankara@gmail.com>
2025-06-24 02:07:09 +0530

    add config file for development

--- config.py
+++ config.py
@@ -2,3 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "devkey")
+    S3_BUCKET = os.environ.get("S3_BUCKET", "medical-reports-summarizer")
+    # Replace with actual API GATEWAY URL
+    API_GATEWAY_WS_URL = os.environ.get("API_GATEWAY_WS_URL",
+                                        "wss://abc123xyz.execute-api.ap-south-1.amazonaws.com/dev")
