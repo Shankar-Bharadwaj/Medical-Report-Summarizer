e41914a51cb24fbb291c9ab130854c71897ccf0c
Shankar Bharadwaj <bharadwajsankara@gmail.com>
2025-06-27 14:14:18 +0530

    Update config.py and folder structure

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "devkey")
-    S3_BUCKET = os.environ.get("S3_BUCKET", "medical-reports-summarizer")
-    # Replace with actual API GATEWAY URL
-    API_GATEWAY_WS_URL = os.environ.get("API_GATEWAY_WS_URL",
-                                        "wss://abc123xyz.execute-api.ap-south-1.amazonaws.com/dev")
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    S3_BUCKET = os.environ.get("S3_BUCKET")
+    API_GATEWAY_WS_URL = os.environ.get("API_GATEWAY_WS_URL")
+    DYNAMO_TABLE = os.environ.get("DYNAMO_TABLE")
